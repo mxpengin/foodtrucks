26996717a11badf146d070247159c4a56cc73782
avalenci <avalenci@kibo.mx>
2024-06-28 02:19:11 -0600

    Displays advanced marker

--- src/gui/SFMap.jsx
+++ src/gui/SFMap.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 //import { Loader } from "@googlemaps/js-api-loader";
-import {APIProvider, Map, useMap} from '@vis.gl/react-google-maps';
+import {APIProvider, Map, useMap, AdvancedMarker} from '@vis.gl/react-google-maps';
 
 
 
@@ -17,55 +17,13 @@ export default function SFMap(){
         console.log("En useEffect-map");
       
 
-
-   
-
       }
     }, [map]);
-
-      /*if (mapRef.current) {
-        const map = new window.google.maps.Map(mapRef.current, {
-          center: { lat: 37.75, lng: -122.41 }, // Coordinates for San Francisco
-          zoom: 12,
-          mapId: "FOODTRUCKS", 
-        });*/
-/*
-        ftdata.forEach(ft => {
-          if(( typeof(ft.location.latitude) === 'number' )&&( typeof(ft.location.longitude ) === 'number' )&&( typeof(ft.applicant) === 'string' )){
-            const marker = new google.maps.marker.AdvancedMarkerElement({
-              map,
-              position: { lat: ft.location.latitude , lng: ft.location.longitude },
-              title: ft.applicant,
-            });  
-          }      
-        });
-*/
-
-   /*   }
-    };*/
-
-    // Ensure the Google Maps API is available
-    /*if (window.google && window.google.maps) {
-      initMap();
-    } else {
-      // Load the Google Maps script if it's not already loaded
-      const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCiYftJxB-6IAs1JryhxvSVRwvq2rsgY8g&libraries=marker`;
-      script.async = true;
-      script.defer = true;
-      script.onload = initMap;
-      document.body.appendChild(script);
-    }*/
-    
-
-
-
-
 
 
 
   return(
-    <div style={{ width: '100vw', height: '100vh' }} ref={mapRef}>
+    <div style={{ width: '95vw', height: '100vh' }} ref={mapRef}>
 
     {/*<APIProvider apiKey="AIzaSyCiYftJxB-6IAs1JryhxvSVRwvq2rsgY8g">*/}
 
@@ -74,8 +32,13 @@ export default function SFMap(){
       defaultCenter={ { lat: 37.75, lng: -122.40 } } 
       style={{ width: '100%', height: '100%' }}
       onLoad={map => setMap(map)}
+      mapId="FOODTRUCKS"
 
-     ></Map>
+     >
+        <AdvancedMarker position={{ lat: 37.75, lng: -122.40 }} title={'Mexicana'}  />
+
+
+     </Map>
       {/*</APIProvider>*/}
       </div>
 
